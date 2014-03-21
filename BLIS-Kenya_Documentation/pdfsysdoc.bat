6dd39d668d4dc9f41e68716a6c2aa426e4226747
ereng <aereng@gmail.com>
2014-03-21 13:02:09 +0300

    bat files for publishing pdf

---
+++ BLIS-Kenya_Documentation/pdfsysdoc.bat
@@ -0,0 +1,1 @@
+fop -xml C:\Users\strath\Documents\BLIS-Docs\BLIS-Kenya_Documentation\Sys-Doc.xml -xsl "C:\Program Files\Syntext\Serna Free 4.4\xml\stylesheets\docbook-xsl-1.68.1\fo\docbook.xsl" -pdf Sys-Doc.pdf
