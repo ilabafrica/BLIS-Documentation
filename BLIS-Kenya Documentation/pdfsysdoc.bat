747c83993cd22ee062578bd6731c1e9995581eca
ereng <aereng@gmail.com>
2014-05-26 15:19:53 +0300

    Remove obsolete files

--- BLIS-Kenya Documentation/pdfsysdoc.bat
+++
@@ -1,1 +0,0 @@
-fop -xml "C:\Users\strath\Documents\BLIS-Docs\BLIS-Kenya Documentation\Sys-Doc.xml" -xsl "C:\Program Files\Syntext\Serna Free 4.4\xml\stylesheets\docbook-xsl-1.68.1\fo\docbook.xsl" -pdf Sys-Doc.pdf
